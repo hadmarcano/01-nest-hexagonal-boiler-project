b5ce4a79052f24f2878f306e44d17db78eefda7a
hadmarcano <hadmarcano@gmail.com>
2025-01-23 00:48:00 -0300

    feat: adding create-user-dto and setting global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  HttpException,
+  HttpStatus,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      disableErrorMessages: false,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            message: errors,
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      },
+    }),
+  );
 
   // app.enableVersioning({
   //   type: VersioningType.URI,
--- src/modules/user/infraestructure/presentation/http/user.controller.ts
+++ src/modules/user/infraestructure/presentation/http/user.controller.ts
@@ -1,10 +1,11 @@
 // User Controller
 import { Controller, Post, Body, HttpException } from '@nestjs/common';
+import { UserCreateDTO } from '../dtos/user.create.dto';
 
 @Controller('users')
 export class UserController {
   @Post()
-  async create(@Body() user: any): Promise<void> {
+  async create(@Body() user: UserCreateDTO): Promise<void> {
     throw new HttpException('Method not implemented', 501);
   }
 }
